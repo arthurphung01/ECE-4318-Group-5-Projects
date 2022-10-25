a109333b5fd8e37e44a090239d29fad8043307ff
NicholasThiem <89762938+NicholasThiem@users.noreply.github.com>
2022-10-25 16:45:18 -0700

    Add files via upload

---
+++ Project 3/Question 2/Cycloid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cycloid
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+        }
+    }
+}
